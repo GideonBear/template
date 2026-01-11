4763ac43b4a7deb5ecc60f6e2d678cc4987b27b1
Gideon <dig-sultry-sedate@duck.com>
2026-01-11 12:12:34 +0100

    add combine.sh

---
+++ combine.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+set -x
+
+if [[ -e "your-cookiecutter" ]]; then
+    echo "Remove the previous your-cookiecutter first"
+    exit 1
+fi
+
+mkdir your-cookiecutter
+
+for arg in "$@"; do
+    rsync -a "$arg/" "your-cookiecutter/"
+done
